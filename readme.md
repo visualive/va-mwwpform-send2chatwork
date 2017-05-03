1ea702c1d5f5313ef47a6e6648f5d19af5fcad60
KUCKLU <kuck1u@visualive.jp>
2017-05-04 03:28:50 +0900

    First commit.

---
+++ readme.md
@@ -0,0 +1,14 @@
+VA MW WP Form send to chatwork
+==============================
+
+Mail sent from "MW WP Form" will be forwarded to room of chatwork.
+
+[![Build Status](https://travis-ci.org/visualive/va-mwwpform-send2chatwork.svg?branch=master)](https://travis-ci.org/visualive/va-mwwpform-send2chatwork)
+
+## Screenshots
+
+### Settings.
+![Settings.](./screenshot-1.png)  
+
+### chatwork room.
+![chatwork room.](./screenshot-2.png)  
---
+++ send2chatwork.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: VA MW WP Form send to chatwork
+ * Plugin URI: https://github.com/visualive/va-mwwpform-send2chatwork
+ * Description: Mail sent from "MW WP Form" will be forwarded to room of chatwork.
+ * Author: KUCKLU
+ * Version: 1.0.0
+ * Author URI: https://www.visualive.jp/
+ * Text Domain: va-mwwpf-send2cw
+ * Domain Path: /langs
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package    WordPress
+ * @subpackage VA MW WP Form send to chatwork
+ * @since      1.0.0
+ * @author     KUCKLU <kuck1u@visualive.jp>
+ * @licenses   GNU General Public License v3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
